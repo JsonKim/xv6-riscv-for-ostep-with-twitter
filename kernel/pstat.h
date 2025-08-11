7992ee8a44658e6952dd111cdf68d8d2280f83c1
Jason Kim <actcrazy@gmail.com>
2025-08-11 23:13:27 +0900

    inuse 변수 타입 변경

--- kernel/pstat.h
+++ kernel/pstat.h
@@ -4,7 +4,7 @@
 #include "param.h"
 
 struct pstat {
-  int inuse[NPROC];   // whether this slot of the process table is in use (1 or 0)
+  _Bool inuse[NPROC];   // whether this slot of the process table is in use (1 or 0)
   uint32 tickets[NPROC]; // the number of tickets this process has
   int pid[NPROC];     // the PID of each process 
   uint32 ticks[NPROC];   // the number of ticks each process has accumulated 
