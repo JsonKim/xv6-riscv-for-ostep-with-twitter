3a90657af725dff1204c217e3dae1c22790db3ee
Jason Kim <actcrazy@gmail.com>
2025-08-11 23:17:33 +0900

    rng state 초기화 개선

--- kernel/rand.c
+++ kernel/rand.c
@@ -11,12 +11,13 @@ static uint64 next_rng_state(uint64 state) {
 
 void
 rng_state_init(uint64* rng_state, uint64 seed) {
-  // 작은 값이 들어오는 경우를 위해 보정한다.
-  // https://chatgpt.com/share/689643df-01a0-8011-9e4f-7c436051ba5c
-  *rng_state = next_rng_state(seed);
   if (!*rng_state) {
     *rng_state = 0x853c49e6748fea9bULL;
   }
+  // 작은 값이 들어오는 경우 보정하기 위해 초기 값을 몇 개 버린다.
+  // https://chatgpt.com/share/689643df-01a0-8011-9e4f-7c436051ba5c
+  pcg32();
+  pcg32();
 }
 
 static uint32
